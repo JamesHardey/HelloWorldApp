e462ec83e9287d4aeaa3f35d042344cd73eb57ca
James <jamesade646@gmail.com>
2021-04-24 16:37:17 -0400

    functions to increment and decrement

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,21 +14,30 @@ class MainActivity : AppCompatActivity() {
 
         val btn1 = findViewById<Button>(R.id.increment) as Button
         val btn2 = findViewById<Button>(R.id.decrement) as Button
-        val numtext = findViewById<TextView>(R.id.number) as TextView
+        val myText = findViewById<TextView>(R.id.number) as TextView
 
-        btn1.setOnClickListener{
-            var num=numtext.text.toString().toInt()
-            num++
-            numtext.text=num.toString()
+
+        btn1.setOnClickListener {
+            incrementNum(myText)
         }
 
-        btn2.setOnClickListener{
-            var num=numtext.text.toString().toInt()
-            if(num>0){
-                num--
-            }
-            numtext.text=num.toString()
+        btn2.setOnClickListener {
+            decrementNum(myText)
         }
 
+    }
+
+    private fun incrementNum(myText:TextView){
+        var num=myText.text.toString().toInt()
+        num++
+        myText.text=num.toString()
+    }
+
+    private fun decrementNum(myText:TextView){
+        var num=myText.text.toString().toInt()
+        if(num>0){
+            num--
+        }
+        myText.text=num.toString()
     }
 }
